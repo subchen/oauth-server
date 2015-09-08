eca695746b3f01ab181ce4861c30c28157a0cbfc
Guoqiang Chen <subchen@gmail.com>
2015-09-08 14:36:45 +0000

    Update

---
+++ webapps/admin/app-bootstrap.js
@@ -0,0 +1,30 @@
+System.config({
+    baseURL: '/',
+    defaultJSExtensions: true,
+    transpiler: 'babel',
+    babelOptions: {
+        'optional': [
+            'runtime',
+            'optimisation.modules.system'
+        ]
+    },
+    map: {
+        'babel': 'node_modules/babel-core/browser.min.js'
+    }
+});
+
+System.shims = function (shims) {
+    Object.keys(shims).forEach(function (name) {
+        System.set(name, System.newModule({
+            'default': shims[name]
+        }));
+    });
+};
+
+System.shims({
+    jquery: window.jQuery,
+    angular: window.angular,
+    lodash: window._
+});
+
+System.import('app-loading.js');
