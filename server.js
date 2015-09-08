53e55c4ad25942ff0895c4db50358e99d79d79b5
Guoqiang Chen <subchen@gmail.com>
2015-09-08 14:53:42 +0000

    Update koa middleware

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var fs = require('co-fs');
+var path = require('path')
 var koa = require('koa');
 var qs = require('koa-qs');
+var bodyParser = require('koa-bodyparser');
+var gzip = require('koa-gzip');
+var staticCache = require('koa-static-cache');
+var json = require('koa-json');
 var router = require('./lib/router');
 
+//
 var app = koa();
 
 // setup koa middleware
 qs(app);
+app.use(staticCache(path.join(__dirname, 'webapps'), {
+  maxAge:24 * 60 * 60
+}))
+app.use(bodyParser());
+app.use(gzip());
+app.use(json({pretty: false}));
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
